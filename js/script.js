bb9f8a5b46f7fb18ce5130ea63496e6beebd1bea
Ahmad Muslih Zain <44734533+aemzayn@users.noreply.github.com>
2020-03-21 12:41:03 +0300

    add time and date

--- js/script.js
+++ js/script.js
@@ -7,8 +7,14 @@ const maghrib = document.getElementById("maghrib");
 const isya = document.getElementById("isya");
 const city = document.getElementById("city");
 
+window.onload = start();
+
+function start() {
+    getDate();
+    getTime();
+}
+
 function getPrayerTime(e) {
-    e.preventDefault();
     var cityName = document.getElementById("cityName").value;
 
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
@@ -42,4 +48,21 @@ function titleCase(str) {
     return str.toLowerCase().split(' ').map(function (word) {
         return (word.charAt(0).toUpperCase() + word.slice(1));
     }).join(' ');
+}
+
+function getDate() {
+    const today = new Date();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const day = days[today.getDay()];
+    const date = `${day}, ${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()}`;
+    document.getElementById("date").textContent = date;
+}
+
+function getTime() {
+    setInterval(() => {
+        const hour = new Date().getHours();
+        const minute = String(new Date().getMinutes());
+        const second = new Date().getSeconds();
+        document.getElementById("time").textContent = `${hour}:${minute}:${second}`
+    }, 1000);
 }
